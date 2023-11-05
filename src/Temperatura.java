ccb314cca5b8e4752d1808d5198e3c26012605dd
filipemonteiro73@gmail.com <filipemonteiro73@gmail.com>
2023-11-05 19:48:51 -0300

    Calculadora F para C

---
+++ src/Temperatura.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Temperatura {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final double div = 5/9.0;
+        final int sub = 32;
+        System.out.println("Qual a temperatura em Fahrenheit?");
+        double tempF = sc.nextDouble();
+        double tempC = (tempF - sub) * div;
+        System.out.printf("A temperatura em Celsius é: %.2f", tempC);
+    }
+}

c066a6f082eddc7375d72f0e2a180aeb7f7f9ed0
filipemonteiro73@gmail.com <filipemonteiro73@gmail.com>
2023-11-05 19:38:15 -0300

    uso de variaveis

---
+++ src/AreaCircunferencia.java
@@ -0,0 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class AreaCircunferencia {
+    public static void main(String[] args) {
+        double raio = 3.4;
+        final double PI = 3.14159;
+        double area = PI * raio * raio;
+        System.out.println(area);
+
+        raio = 10;
+        area = PI *Math.pow(raio,2);
+        System.out.println("Area = "+ area+ "m2.");
+    }
+}

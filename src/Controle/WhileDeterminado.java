4425cadd2f7075407f7db143ff1a6d8198d70860
filipemonteiro73@gmail.com <filipemonteiro73@gmail.com>
2023-11-12 13:36:51 -0300

    While

---
+++ src/Controle/WhileDeterminado.java
@@ -0,0 +1,12 @@
+package Controle;
+
+public class WhileDeterminado {
+    public static void main(String[] args) {
+        int contador =0;
+
+        while(contador < 10){
+            System.out.println("Bom dia!");
+            contador ++;
+        }
+    }
+}

55a2d76293f05ad87d10bbf25faba03f3d40abf3
filipemonteiro73@gmail.com <filipemonteiro73@gmail.com>
2023-11-12 13:45:14 -0300

    For

---
+++ src/Controle/For1.java
@@ -0,0 +1,15 @@
+package Controle;
+
+public class For1 {
+    public static void main(String[] args) {
+        int contador =1;
+
+        while(contador <= 10){
+            System.out.println("Bom dia!");
+            contador ++;
+        }
+        for(int i =1; i <= 10; i++){
+            System.out.println("Bom dia!");
+        }
+    }
+}

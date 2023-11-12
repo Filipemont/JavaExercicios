c12c1a209bef226658d3fabb63d3e3c8d3e8eb36
filipemonteiro73@gmail.com <filipemonteiro73@gmail.com>
2023-11-12 12:27:14 -0300

    IfElse

--- src/Controle/If.java
+++ src/Controle/If.java
@@ -12,9 +12,11 @@ public class If {
         if(media <= 10 && media>=7){
             System.out.println("Aprovado");
             System.out.println("Parabéns");
-        } else if (media < 7 && media >= 4.5) {
+        }
+        if (media < 7 && media >= 4.5) {
             System.out.println("Recuperação");
-        }else if( media < 4.5 && media >=0){
+        }
+        if( media < 4.5 && media >=0){
             System.out.println("Reprovado");
         }
     }
